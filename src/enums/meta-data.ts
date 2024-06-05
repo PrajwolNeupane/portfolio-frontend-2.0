d6fe953d8593b99c841e6e35a7919f91b8bc6be8
PrajwolNeupane <prajwolneupane68@gmail.com>
2024-06-06 00:18:33 +0545

    working meta data

---
+++ src/enums/meta-data.ts
@@ -0,0 +1,30 @@
+import { Metadata } from "next";
+
+export const mainMetaData: Metadata = {
+  title: "Prajwol Neupane - Frontend Developer Portfolio",
+  description:
+    "Welcome to Prajwol Neupane's portfolio. Explore my projects, skills, and experience in frontend development using React, Next.js, and more.",
+  openGraph: {
+    title: "Prajwol Neupane - Frontend Developer Portfolio",
+    description:
+      "Welcome to Prajwol Neupane's portfolio. Explore my projects, skills, and experience in frontend development using React, Next.js, and more.",
+    type: "website",
+    url: "prajwolneupane.com.np",
+    images: [
+      {
+        url: "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/387825225_2038564676542674_3981752417253564040_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kgr9wVSMqaIQ7kNvgFCgcR_&_nc_ht=scontent.fktm3-1.fna&oh=00_AYBgDyosLypKALqKCOChJOKwQ_lY8KPUn7Cnarg2gNGoZQ&oe=66667F56",
+        width: 800,
+        height: 600,
+        alt: "Prajwol Neupane's Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Prajwol Neupane - Frontend Developer Portfolio",
+    description:
+      "Welcome to Prajwol Neupane's portfolio. Explore my projects, skills, and experience in frontend development using React, Next.js, and more.",
+    images:
+      "https://scontent.fktm3-1.fna.fbcdn.net/v/t39.30808-6/387825225_2038564676542674_3981752417253564040_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kgr9wVSMqaIQ7kNvgFCgcR_&_nc_ht=scontent.fktm3-1.fna&oh=00_AYBgDyosLypKALqKCOChJOKwQ_lY8KPUn7Cnarg2gNGoZQ&oe=66667F56",
+  },
+};
